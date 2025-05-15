05899d3d700e6126a71d3d4d85b7b73133e314b8
Yoran <49372904+Y0RAN@users.noreply.github.com>
2025-05-15 12:13:08 +0200

    A3 compleet

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -5,8 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Animation;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -18,43 +20,57 @@ namespace PRA_B4_FOTOKIOSK.controller
 
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-        
-        
+
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
             var now = DateTime.Now;
             int day = (int)now.DayOfWeek;
-            
-            // Initializeer de lijst met fotos
-            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
-            // foreach is een for-loop die door een array loopt
+
             foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
             {
-                if (!(dir.Split("\\")[1].StartsWith(day.ToString()))) continue;
-                
+                if (!(dir.Split("\\")[1].StartsWith(day.ToString()))) continue; // check juiste dag
+
                 foreach (string file in Directory.GetFiles(dir))
                 {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-
-
-                    var fileDate = DateTime.Parse(file.Split("\\")[2].Split("_id")[0].Replace("_", ":"));
-                    if (fileDate >= now.AddMinutes(-30) && fileDate <= now.AddMinutes(-2))
+                    var fileDate = DateTime.Parse(file.Split("\\")[2].Split("_id")[0].Replace("_", ":")); // van bestand naar datetime
+                    if (fileDate >= now.AddMinutes(-30) && fileDate <= now.AddMinutes(-2)) // tussen 2 en 30 minuten geleden
                     {
-                        PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+
+                        bool added = false;
+                        foreach (KioskPhoto photo in PicturesToDisplay)
+                        {
+
+                            var photoDate = DateTime.Parse(photo.Source.Split("\\")[2].Split("_id")[0].Replace("_", ":")); // van foto naar datetime
+                            if (photoDate.AddSeconds(60) != fileDate) continue; // check of het precies 60 verschil is
+
+                            int index = PicturesToDisplay.IndexOf(photo);
+                            PicturesToDisplay.Insert(index, new KioskPhoto() { Id = 0, Source = file });
+                            added = true;
+                            break;
+                        }
+
+                        if (!added)
+                        {
+                            PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+
+                        }
 
                     }
 
 
-                }
-            }
 
+
+
+                }
+                
+
+            }
             // Update de fotos
             PictureManager.UpdatePictures(PicturesToDisplay);
         }
+
+       
 
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
